7ec96c92483024a6c2ee9679b8d0bc1f34fbe139
Tyler Reynolds <me@reynolds.tj>
2021-03-11 00:12:04 -0800

    Add AnimationInstance unit tests
    A good portion of these tests don't pass currently.

--- src/OpenSage.Game/Logic/Object/Draw/W3dModelDraw.cs
+++ src/OpenSage.Game/Logic/Object/Draw/W3dModelDraw.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
@@ -178,7 +178,7 @@ namespace OpenSage.Logic.Object
                     {
                         var flags = animationState.Flags;
                         var mode = animationBlock.AnimationMode;
-                        var animationInstance = new AnimationInstance(modelInstance, anim, mode, flags, GameObject);
+                        var animationInstance = new AnimationInstance(modelInstance.ModelBoneInstances, anim, mode, flags);
                         modelInstance.AnimationInstances.Add(animationInstance);
                         animationInstance.Play();
                         break;
@@ -270,7 +270,7 @@ namespace OpenSage.Logic.Object
                 {
                     var mode = conditionState.AnimationMode;
                     var flags = conditionState.Flags;
-                    var animationInstance = new AnimationInstance(modelInstance, animation, mode, flags, GameObject);
+                    var animationInstance = new AnimationInstance(modelInstance.ModelBoneInstances, animation, mode, flags);
                     modelInstance.AnimationInstances.Add(animationInstance);
                 }
             }
